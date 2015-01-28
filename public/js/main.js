99651ffa916388a8a25efe8aad7701feea8eaf27
Jorge Torres <jtorrescolan@gmail.com>
2015-01-28 16:20:37 -0500

    OK

--- public/js/main.js
+++ public/js/main.js
@@ -3,25 +3,56 @@ var INDEX = (function(){
 	var fnCambiarTamanio = function(){
 		var height = window.innerHeight;
 		document.getElementById('cabecera').style.height = height+'px';
+		document.querySelector('.contenedor-menu').style.height = (height-93)+'px';
+
 	}
 
 	var fnEventos = function(){
 		window.onresize = function(){
 			var height = window.innerHeight;
 			document.getElementById('cabecera').style.height = height+'px';
+			document.querySelector('.contenedor-menu').style.height = (height-93)+'px';
 		}
 	}
 
 	var fnCargarMapa = function(){
 		var mapOptions={
-			zoom: 8,
-			center: new google.maps.LatLng()
+			zoom: 16,
+			center: new google.maps.LatLng(-12.0895883,-77.0311585),
+			scrollwheel: false
 		}
 		var map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+		var marker  = new google.maps.Marker({
+			position: mapOptions.center,
+			map: map
+		});
 	}
 
 	var fnCargarMenuPrincipal = function(){
 
+		var menu_principal = document.getElementById('boton-menu'),
+			botones = menu_principal.getElementsByTagName('i');
+			contenedor_menu = document.querySelector('.contenedor-menu');
+
+		menu_principal.onclick = function(){
+
+			if(menu_principal.getAttribute('activado')==='false'){
+				document.querySelector('.cabecera-principal').style.backgroundColor = 'rgb(45, 48, 53)';
+				botones[0].style.display = 'none';
+				botones[1].style.display = 'block';
+				menu_principal.setAttribute('activado', 'true');
+				contenedor_menu.style.display = 'block';
+				document.body.style.overflowY = 'hidden'
+			}
+			else{
+				document.querySelector('.cabecera-principal').style.backgroundColor = 'transparent';
+				botones[0].style.display = 'block';
+				botones[1].style.display = 'none';
+				menu_principal.setAttribute('activado', 'false');
+				contenedor_menu.style.display = 'none';
+				document.body.style.overflowY = 'auto'	
+			}
+		}
 	}
 
 	return{
@@ -40,6 +71,7 @@ window.onload= function(){
 	INDEX.fnEventos();
 	INDEX.fnCambiarTamanio();
 	INDEX.fnCargarMapa();
+	INDEX.fnCargarMenuPrincipal();
 
 	/* Fin Página inicial*/
 }
